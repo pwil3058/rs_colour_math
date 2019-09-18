38ad2475667a67b884d70d1325d45c6cee6455ea
Peter Williams <pwil3058@gmail.com>
2019-09-18 16:35:56 +1000

    Implement index access to components
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/rgb.rs
+++ src/rgb.rs
@@ -1,5 +1,7 @@
 // Copyright 2019 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
+use std::ops::Index;
+
 use num::traits::{Float, FloatConst, NumAssign, NumOps};
 
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
@@ -63,5 +65,52 @@ impl<F: Float + FloatConst + NumAssign + NumOps> RGB<F> {
 
     pub fn blue_component(self) -> F {
         self.0[Self::I_BLUE]
+    }
+
+    pub fn indices_value_order(self) -> [usize; 3] {
+        if self.0[0] >= self.0[1] {
+            if self.0[0] >= self.0[2] {
+                if self.0[1] >= self.0[2] {
+                    [0, 1, 2]
+                } else {
+                    [0, 2, 1]
+                }
+            } else {
+                [2, 0, 1]
+            }
+        } else if self.0[1] >= self.0[2] {
+            if self.0[0] >= self.0[2] {
+                [1, 0, 2]
+            } else {
+                [1, 2, 0]
+            }
+        } else {
+            [2, 1, 0]
+        }
+    }
+}
+
+impl<F: Float + FloatConst + NumAssign + NumOps> Index<usize> for RGB<F> {
+    type Output = F;
+
+    fn index(&self, index: usize) -> &F {
+        &self.0[index]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn indices_order() {
+        assert_eq!(RGB::<f64>::WHITE.indices_value_order(), [0, 1, 2]);
+        assert_eq!(RGB::<f64>::BLACK.indices_value_order(), [0, 1, 2]);
+        assert_eq!(RGB::<f64>::RED.indices_value_order(), [0, 1, 2]);
+        assert_eq!(RGB::<f64>::GREEN.indices_value_order(), [1, 0, 2]);
+        assert_eq!(RGB::<f64>::BLUE.indices_value_order(), [2, 0, 1]);
+        assert_eq!(RGB::<f64>::CYAN.indices_value_order(), [1, 2, 0]);
+        assert_eq!(RGB::<f64>::MAGENTA.indices_value_order(), [0, 2, 1]);
+        assert_eq!(RGB::<f64>::YELLOW.indices_value_order(), [0, 1, 2]);
     }
 }
