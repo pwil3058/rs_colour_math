df55a4c9f4aa62e85d6732dddab0811bb592d68a
Peter Williams <pwil3058@gmail.com>
2019-09-19 09:13:41 +1000

    Generalize colour constant definitions
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/rgb.rs
+++ src/rgb.rs
@@ -4,27 +4,27 @@ use std::ops::Index;
 
 use num::traits::{Float, FloatConst, NumAssign, NumOps};
 
+pub trait RGBConstants: Sized {
+    const RED: Self;
+    const GREEN: Self;
+    const BLUE: Self;
+
+    const CYAN: Self;
+    const MAGENTA: Self;
+    const YELLOW: Self;
+
+    const WHITE: Self;
+    const BLACK: Self;
+
+    const PRIMARIES: [Self; 3] = [Self::RED, Self::GREEN, Self::BLUE];
+    const SECONDARIES: [Self; 3] = [Self::CYAN, Self::MAGENTA, Self::YELLOW];
+    const GREYS: [Self; 2] = [Self::BLACK, Self::WHITE];
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct RGB<F: Float + FloatConst + NumAssign + NumOps>([F; 3]);
 
-pub trait RGBConstants<F: Float + FloatConst + NumAssign + NumOps> {
-    const RED: RGB<F>;
-    const GREEN: RGB<F>;
-    const BLUE: RGB<F>;
-
-    const CYAN: RGB<F>;
-    const MAGENTA: RGB<F>;
-    const YELLOW: RGB<F>;
-
-    const WHITE: RGB<F>;
-    const BLACK: RGB<F>;
-
-    const PRIMARIES: [RGB<F>; 3] = [Self::RED, Self::GREEN, Self::BLUE];
-    const SECONDARIES: [RGB<F>; 3] = [Self::CYAN, Self::MAGENTA, Self::YELLOW];
-    const GREYS: [RGB<F>; 2] = [Self::BLACK, Self::WHITE];
-}
-
-impl RGBConstants<f32> for RGB<f32> {
+impl RGBConstants for RGB<f32> {
     const RED: RGB::<f32> = RGB([1.0, 0.0, 0.0]);
     const GREEN: RGB::<f32> = RGB([0.0, 1.0, 0.0]);
     const BLUE: RGB::<f32> = RGB([0.0, 0.0, 1.0]);
@@ -37,7 +37,7 @@ impl RGBConstants<f32> for RGB<f32> {
     const BLACK: RGB::<f32> = RGB([0.0, 0.0, 0.0]);
 }
 
-impl RGBConstants<f64> for RGB<f64> {
+impl RGBConstants for RGB<f64> {
     const RED: RGB::<f64> = RGB([1.0, 0.0, 0.0]);
     const GREEN: RGB::<f64> = RGB([0.0, 1.0, 0.0]);
     const BLUE: RGB::<f64> = RGB([0.0, 0.0, 1.0]);
