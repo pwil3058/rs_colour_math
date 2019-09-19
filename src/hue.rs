4b489825afb14cc8ad3418fe2a69357773a2c826
Peter Williams <pwil3058@gmail.com>
2019-09-20 07:45:44 +1000

    Accommodate changes to normalised_angles
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/hue.rs
+++ src/hue.rs
@@ -1,31 +1,25 @@
 // Copyright 2019 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
 use normalised_angles::{Angle, AngleConst};
-use num::traits::{Float, FloatConst, NumAssign, NumOps};
+use num::traits::{Float, NumAssign, NumOps};
 
 use crate::rgb::PRGB;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-pub struct HueAngle<F: Float + FloatConst + NumAssign + NumOps> {
+pub struct HueAngle<F: Float + NumAssign + NumOps + AngleConst> {
     angle: Angle<F>,
     max_chroma_rgb: PRGB<F>,
     chroma_correction: F,
 }
 
-impl<F: Float + FloatConst + NumAssign + NumOps> HueAngle<F> {
-    fn calc_other(abs_angle: Angle<F>) -> F
-    where
-        Angle<F>: AngleConst,
-    {
+impl<F: Float + NumAssign + NumOps + AngleConst> HueAngle<F> {
+    fn calc_other(abs_angle: Angle<F>) -> F {
         if [Angle::<F>::DEG_0, Angle::<F>::DEG_120].contains(&abs_angle) {
             F::from(0.0).unwrap()
         } else if [Angle::<F>::DEG_60, Angle::<F>::DEG_180].contains(&abs_angle) {
             F::from(1.0).unwrap()
         } else {
-            fn f<F: Float + FloatConst + NumAssign + NumOps>(angle: Angle<F>) -> F
-            where
-                Angle<F>: AngleConst,
-            {
+            fn f<F: Float + NumAssign + NumOps + AngleConst>(angle: Angle<F>) -> F {
                 // Careful of float not fully representing reals
                 (angle.sin() / (Angle::<F>::DEG_120 - angle).sin()).min(F::from(1.0).unwrap())
             };
