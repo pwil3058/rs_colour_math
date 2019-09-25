ea067c9f796b565aa2733206395c19a14941e759
Peter Williams <pwil3058@gmail.com>
2019-09-25 11:25:49 +1000

    Test using new calc_other() function from angles
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/chroma.rs
+++ src/chroma.rs
@@ -22,13 +22,14 @@ mod test {
     use crate::rgb::*;
     use crate::ColourComponent;
     use float_cmp::*;
+    use normalised_angles::Degrees;
 
     const NON_ZERO_VALUES: [f64; 7] = [0.000000001, 0.025, 0.5, 0.75, 0.9, 0.99999, 1.0];
 
     #[test]
-    fn calc_other() {
+    fn calc_other_from_rgb() {
         for val in NON_ZERO_VALUES.iter() {
-            for ratio in [0.01, 0.25, 0.5, 0.75, 0.99].iter() {
+            for ratio in NON_ZERO_VALUES.iter() {
                 let mod_val = val * *ratio;
                 for (array, expected) in &[
                     ([*val, 0.0, 0.0], 0.0),
@@ -50,6 +51,47 @@ mod test {
                     assert!(approx_eq!(f64, other, *expected, epsilon = 0.00000000001));
                 }
             }
+        }
+    }
+
+    #[test]
+    fn calc_other_from_angle() {
+        for (angle, expected) in &[
+            (f64::RED_ANGLE, 0.0),
+            (f64::GREEN_ANGLE, 0.0),
+            (f64::BLUE_ANGLE, 0.0),
+            (f64::CYAN_ANGLE, 1.0),
+            (f64::MAGENTA_ANGLE, 1.0),
+            (f64::YELLOW_ANGLE, 1.0),
+            (-180.0, 1.0),
+            (-165.0, 2.0 / (f64::SQRT_3 + 1.0)),
+            (-150.0, 0.5),
+            (-135.0, (f64::SQRT_3 - 1.0) / (f64::SQRT_3 + 1.0)),
+            (-120.0, 0.0),
+            (-105.0, (f64::SQRT_3 - 1.0) / (f64::SQRT_3 + 1.0)),
+            (-90.0, 0.5),
+            (-75.0, 2.0 / (f64::SQRT_3 + 1.0)),
+            (-60.0, 1.0),
+            (-45.0, 2.0 / (f64::SQRT_3 + 1.0)),
+            (-30.0, 0.5),
+            (-15.0, (f64::SQRT_3 - 1.0) / (f64::SQRT_3 + 1.0)),
+            (0.0, 0.0),
+            (15.0, (f64::SQRT_3 - 1.0) / (f64::SQRT_3 + 1.0)),
+            (30.0, 0.5),
+            (45.0, 2.0 / (f64::SQRT_3 + 1.0)),
+            (60.0, 1.0),
+            (75.0, 2.0 / (f64::SQRT_3 + 1.0)),
+            (90.0, 0.5),
+            (105.0, (f64::SQRT_3 - 1.0) / (f64::SQRT_3 + 1.0)),
+            (120.0, 0.0),
+            (135.0, (f64::SQRT_3 - 1.0) / (f64::SQRT_3 + 1.0)),
+            (150.0, 0.5),
+            (165.0, 2.0 / (f64::SQRT_3 + 1.0)),
+        ] {
+            let hue_angle = Degrees::<f64>::from(*angle);
+            let other = super::calc_other(hue_angle.xy());
+            assert!(other.is_proportion(), "other = {}", other);
+            assert!(approx_eq!(f64, other, *expected, epsilon = 0.00000000001));
         }
     }
 }
