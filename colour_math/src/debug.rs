dafb90a1dd6d4d00ca11da842cf5b03803c6f517
Peter Williams <pwil3058@bigpond.net.au>
2021-03-30 10:20:49 +1000

    colour_math::debug: guard against divide by zero
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- colour_math/src/debug.rs
+++ colour_math/src/debug.rs
@@ -18,6 +18,7 @@ impl AbsDiff for u16 {}
 impl AbsDiff for u32 {}
 impl AbsDiff for u64 {}
 impl AbsDiff for u128 {}
+
 impl AbsDiff for f32 {}
 impl AbsDiff for f64 {}
 
@@ -25,47 +26,52 @@ pub trait PropDiff {
     fn prop_diff(&self, other: &Self) -> Prop;
 }
 
-impl PropDiff for u8 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) as u128 * u64::MAX as u128 / *self.max(other) as u128) as u64)
-    }
+macro_rules! impl_prop_diff_for_unsigned {
+    (u128) => {
+        impl PropDiff for u128 {
+            fn prop_diff(&self, other: &Self) -> Prop {
+                match self.max(other) {
+                    0 => Prop::ZERO,
+                    denom => Prop((self.abs_diff(other) * u64::MAX as u128 / denom) as u64),
+                }
+            }
+        }
+    };
+    ($unsigned:ty) => {
+        impl PropDiff for $unsigned {
+            fn prop_diff(&self, other: &Self) -> Prop {
+                match *self.max(other) as u128 {
+                    0 => Prop::ZERO,
+                    denom => Prop((self.abs_diff(other) as u128 * u64::MAX as u128 / denom) as u64),
+                }
+            }
+        }
+    };
 }
 
-impl PropDiff for u16 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) as u128 * u64::MAX as u128 / *self.max(other) as u128) as u64)
-    }
+impl_prop_diff_for_unsigned!(u8);
+impl_prop_diff_for_unsigned!(u16);
+impl_prop_diff_for_unsigned!(u32);
+impl_prop_diff_for_unsigned!(u64);
+impl_prop_diff_for_unsigned!(u128);
+
+macro_rules! impl_prop_diff_for_float {
+    ($float:ty) => {
+        impl PropDiff for $float {
+            fn prop_diff(&self, other: &Self) -> Prop {
+                let denom = self.max(*other);
+                if denom == 0.0 {
+                    Prop::ZERO
+                } else {
+                    Prop((self.abs_diff(other) * u64::MAX as $float / denom) as u64)
+                }
+            }
+        }
+    };
 }
 
-impl PropDiff for u32 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) as u128 * u64::MAX as u128 / *self.max(other) as u128) as u64)
-    }
-}
-
-impl PropDiff for u64 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) as u128 * u64::MAX as u128 / *self.max(other) as u128) as u64)
-    }
-}
-
-impl PropDiff for u128 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) * u64::MAX as u128 / self.max(other)) as u64)
-    }
-}
-
-impl PropDiff for f32 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) * u64::MAX as f32 / self.max(*other)) as u64)
-    }
-}
-
-impl PropDiff for f64 {
-    fn prop_diff(&self, other: &Self) -> Prop {
-        Prop((self.abs_diff(other) * u64::MAX as f64 / self.max(*other)) as u64)
-    }
-}
+impl_prop_diff_for_float!(f32);
+impl_prop_diff_for_float!(f64);
 
 pub trait ApproxEq: Copy + PropDiff + PartialEq {
     const DEFAULT_MAX_DIFF: Prop = Prop(0x0000000000001000);
@@ -85,15 +91,10 @@ pub trait ApproxEq: Copy + PropDiff + PartialEq {
 }
 
 impl ApproxEq for u8 {}
-
 impl ApproxEq for u16 {}
-
 impl ApproxEq for u32 {}
-
 impl ApproxEq for u64 {}
-
 impl ApproxEq for u128 {}
 
 impl ApproxEq for f32 {}
-
 impl ApproxEq for f64 {}
