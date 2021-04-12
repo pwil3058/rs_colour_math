f70060498bc8987674f1e836f1cc765e574d594d
Peter Williams <pwil3058@bigpond.net.au>
2021-04-12 13:05:37 +1000

    colour_math::hue: test lightest/darkest RGBs for nominated Chroma.
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- colour_math_real/src/beigui/test_beigui.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
-
-use crate::fdrn::FDRNumber;
-
-#[test]
-fn div_by_u8() {
-    assert_eq!(FDRNumber::ONE / 2, FDRNumber(u64::MAX as i128 / 2));
-    assert_eq!(FDRNumber::ONE / 3, FDRNumber(u64::MAX as i128 / 3));
-}
